ac3bae694688f1dd2138b36cb32e4642c24574e5
Curtis Gray <87861225+curtisgry@users.noreply.github.com>
2022-02-28 16:12:01 -0500

    add existing files to git

---
+++ server.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
+http.createServer((request, response) => {
+        const addr = request.url;
+        const q = url.parse(addr, true);
+        let filePath = '';
+
+        fs.appendFile('log.txt', `URL : ${addr} \n Timestamp: ${new Date()} \n\n`, (err) => {
+                if (err) {
+                        console.log(err);
+                } else {
+                        console.log('Added to log.');
+                }
+        });
+
+        if (q.pathname.includes('documentation')) {
+                filePath = `${__dirname}/documentation.html`;
+        } else {
+                filePath = 'index.html';
+        }
+
+        fs.readFile(filePath, (err, data) => {
+                if (err) throw err;
+                response.writeHead(200, { 'Content-Type': 'text/html' });
+                response.write(data);
+                response.end();
+        });
+}).listen(8080);
+
+console.log('My first Node test server is running on port 8080.');
