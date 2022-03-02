49c6a56f7680505a2225f693050280db7e6c2484
Curtis Gray <87861225+curtisgry@users.noreply.github.com>
2022-03-02 16:46:54 -0500

    add test movie data

--- index.js
+++ index.js
@@ -1,6 +1,51 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const movieData = [
+        {
+                title: 'Interstellar',
+                year: '2014',
+        },
+        {
+                title: 'Arrival',
+                year: '2016',
+        },
+        {
+                title: 'The Matrix',
+                year: '1999',
+        },
+        {
+                title: 'Star Wars: A New Hope',
+                year: '1977',
+        },
+        {
+                title: 'Goodfellas',
+                year: '1990',
+        },
+        {
+                title: 'Catch Me If You Can',
+                year: '2002',
+        },
+        {
+                title: 'Spirited Away',
+                year: '2001',
+        },
+        {
+                title: 'Good Will Hunting',
+                year: '1997',
+        },
+        {
+                title: 'The Dark Knight',
+                year: '2008',
+        },
+        {
+                title: 'The Truman Show',
+                year: '1998',
+        },
+];
+
 const app = express();
 
 app.use(morgan('common'));
+
+app.get('/movies', (req, res) => {});
