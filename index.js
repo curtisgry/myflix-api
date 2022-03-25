51c131758f6fe3906b1893a86d9e3f2f1cdd378b
Curtis Gray <87861225+curtisgry@users.noreply.github.com>
2022-03-25 15:17:05 -0400

    update index.js for heroku deploy

--- index.js
+++ index.js
@@ -244,6 +244,8 @@ app.use((err, req, res, next) => {
         res.status(500).send('Something went wrong');
 });
 
-app.listen(8080, () => {
-        console.log('Listening on port 8080');
+const port = process.env.PORT || 8080;
+
+app.listen(port, '0,0,0,0', () => {
+        console.log(`Listening on port ${port}`);
 });
